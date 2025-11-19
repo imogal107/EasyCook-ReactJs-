58d89e420b8f8250d3599821f485a0561eb342d9
imogal107 <izmogal111@gmail.com>
2025-11-19 22:22:48 +0530

    Deployment update

--- src/pages/FavouritesPage.jsx
+++ src/pages/FavouritesPage.jsx
@@ -8,11 +8,11 @@ const FavouritesPage = () => {
     <div className=" flex-1 p-10 min-h-screen bg-slate-200">
       <div className=" max-w-screen-lg mx-auto">
         <p className=" p-4 text-3xl md:text-5xl font-bold">My Favourites</p>
-        {favourites.length === 0 ? (
+        {favourites.length === 0 && (
           <div className="flex flex-col items-center gap-4 h-[70vh]">
             <img src="/404.svg" alt="404 image" className="h-3/4" />
           </div>
-        ) : (
+        )} 
           <div className="border-2 grid justify-center items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full  pl-2 pr-6 ">
             {
               favourites.map((recipe)=>(
@@ -20,7 +20,7 @@ const FavouritesPage = () => {
               ))
             }
           </div>
-        )}
+       
       </div>
     </div>
   );
